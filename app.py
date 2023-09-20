463bc60291181321727f87710c0c149d943d8180
YashSunidhi <smnitrkl50@gmail.com>
2023-09-20 23:09:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 import streamlit as st
-from trubrics_utils import trubrics_config, trubrics_successful_feedback
+#from trubrics_utils import trubrics_config, trubrics_successful_feedback
 
 from trubrics.integrations.streamlit import FeedbackCollector
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+def trubrics_config(default_component: bool = True):
+    st.subheader("Input your Trubrics credentials:")
+    email = st.text_input(
+        label="email", placeholder="email", label_visibility="collapsed", value=st.secrets.get("TRUBRICS_EMAIL", "")
+    )
+
+    password = st.text_input(
+        label="password",
+        placeholder="password",
+        label_visibility="collapsed",
+        type="password",
+        value=st.secrets.get("TRUBRICS_PASSWORD", ""),
+    )
+
+    if default_component:
+        return email, password
+
+    feedback_component = st.text_input(
+        label="feedback_component",
+        placeholder="Feedback component name",
+        label_visibility="collapsed",
+    )
+
+    feedback_type = st.radio(
+        label="Select the component feedback type:", options=("faces", "thumbs", "textbox"), horizontal=True
+    )
+
+    return email, password, feedback_component, feedback_type
 
 model_name_or_path = "TheBloke/WizardLM-13B-V1.2-GPTQ"
 #model_name_or_path = 'togethercomputer/LLaMA-2-7B-32K'
