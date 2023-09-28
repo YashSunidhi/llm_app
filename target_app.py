b73dd2a3d8ee3227dbb0686afbe4073eef1b36eb
YashSunidhi <smnitrkl50@gmail.com>
2023-09-28 22:39:10 +0200

    Update target_app.py

--- target_app.py
+++ target_app.py
@@ -180,7 +180,6 @@ with response_container:
                     st.write("#### Raw feedback saved to Trubrics:")
                     st.write(feedback)
 
-st.sidebar.button('Reset Chat', on_click=reset_conversation)
 
                     # Clear the Chat Messages
 
