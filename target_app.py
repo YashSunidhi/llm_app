1a69776fe1a0037d8877caa190e34f7ec9aea8a3
YashSunidhi <smnitrkl50@gmail.com>
2023-09-28 22:05:34 +0200

    Update target_app.py

--- target_app.py
+++ target_app.py
@@ -140,7 +140,7 @@ with response_container:
                 st.session_state.response = response_text
                 st.session_state.feedback_key += 1
 
-        st.sidebar.button('Reset Chat', on_click=reset_conversation, key=i)
+            
                 #tab1, tab2, tab3 = st.tabs(["Generated Outcome 1","Generated Outcome 2","Generated Outcome 3"])
                 # try:
                 #     tab1, tab2, tab3 = st.tabs(["Generated Outcome 1","Generated Outcome 2","Generated Outcome 3"])
@@ -171,6 +171,7 @@ with response_container:
                 if feedback:
                     st.write("#### Raw feedback saved to Trubrics:")
                     st.write(feedback)
+                st.sidebar.button('Reset Chat', on_click=reset_conversation, key=i)
 
                     # Clear the Chat Messages
 
