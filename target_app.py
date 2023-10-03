bb4d6c86380e4cd7e508bc4de99ffab3045ec50f
YashSunidhi <smnitrkl50@gmail.com>
2023-10-03 15:56:47 +0200

    Update target_app.py

--- target_app.py
+++ target_app.py
@@ -151,13 +151,13 @@ with response_container:
                 try:
                     tab1, tab2, tab3 = st.tabs(["French Translation","German Translation","Spanish Translation"])
                     with tab1:
-                        response1= tab1.write(generate_response(f''' translate the context in french {response_text} ''')['generated'])
+                        response1= tab1.write(generate_response(f''' translate the context in french {str('""" ')+ response_text + str(' """')} ''')['generated'][0])
                         st.markdown(response1)
                     with tab2:
-                        response2= tab2.write(generate_response(f''' translate the context in german {response_text} ''')['generated'])
+                        response2= tab2.write(generate_response(f''' translate the context in german {str('""" ')+ response_text + str(' """')} ''')['generated'][0])
                         st.markdown(response2)
                     with tab3:
-                        response3= tab3.write(generate_response(f''' translate the context in spanish {response_text} ''')['generated'])
+                        response3= tab3.write(generate_response(f''' translate the context in spanish {str('""" ')+ response_text + str(' """')} ''')['generated'][0])
                         st.markdown(response3)
                 except:
                     pass
