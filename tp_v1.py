533de24f21a0214019eb0181d957c775f05f1feb
YashSunidhi <smnitrkl50@gmail.com>
2023-10-06 01:09:15 +0200

    Update tp_v1.py

--- tp_v1.py
+++ tp_v1.py
@@ -148,7 +148,7 @@ def text_gen():
     try:
         df = pd.DataFrame(st.session_state.messages[-1]['content'])
     except:
-        pass
+        df = pd.DataFrame()
     # def convert_df(df):
     #     return df.to_csv(sep='\t', index=False)#index=False).encode('utf-8')
 
