cc53a10d16a6124591139b992c4f4e8e5379158d
YashSunidhi <smnitrkl50@gmail.com>
2023-10-06 00:24:34 +0200

    Update tp_v1.py

--- tp_v1.py
+++ tp_v1.py
@@ -142,18 +142,18 @@ def text_gen():
                     st.write(str(count)+ str(": "), source.title, source.link,source.hostname)
         message = {"role": "assistant", "content": response}
         st.session_state.messages.append(message)
-    df = pd.DataFrame(st.session_state.messages)
+    df = pd.DataFrame(st.session_state.messages[-1]['content'], columns = ['content'])
     def convert_df(df):
-       return df.to_csv()#index=False).encode('utf-8')
+       return df.to_csv(sep='\t', index=False)#index=False).encode('utf-8')
     
     csv = convert_df(df)
     
     st.download_button(
        "Press to Download and save",
        csv,
-       "file.csv",
+       "file.txt",
        "text/csv",
-       key='download-csv'
+       key='download-txt'
     )
     
 
