d6368f432c08cc5ed1078be9af8c43a77606a643
YashSunidhi <smnitrkl50@gmail.com>
2023-10-06 09:52:36 +0200

    Update tp_v1.py

--- tp_v1.py
+++ tp_v1.py
@@ -119,7 +119,7 @@ def text_gen():
                 string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     
         prompt = f"{string_dialogue} {prompt_input} Assistant: "
-        return chatbot.query(prompt,web_search=True, truncate = 8192,max_new_tokens= 2048)
+        return chatbot.query(prompt,web_search=True,max_new_tokens= 4096)
     
     
     # User-provided prompt
