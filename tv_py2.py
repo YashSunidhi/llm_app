997627fa3de7490ad4d2d81e38d6d4ec123310d7
YashSunidhi <smnitrkl50@gmail.com>
2023-10-14 02:46:20 +0200

    Update tv_py2.py

--- tv_py2.py
+++ tv_py2.py
@@ -399,13 +399,14 @@ def image_gen():
             option7 = st.selectbox('Recommended feedback here',("","Create a very high quality image. "," Try emphasizing on facial expression."))
             option9 = st.text_input("Insert Your feedback","")
 
-            default_prompt = [ option6 + str(" ")+ option1 + str(", ") +  option2+  str(", ")+ option3+  str(", ")+ option4+  str(", ")+ option5+ str(", ")+option7 + str(" ") +option9]
-        else:
-            default_prompt = ["A photograph of a doctor or healthcare professional in a clinical setting, looking compassionate and confident while interacting with a patient. This image should convey a sense of trust and expertise."]
+    if option6:
+        default_prompt = [ option6 + str(" ")+ option1 + str(", ") +  option2+  str(", ")+ option3+  str(", ")+ option4+  str(", ")+ option5+ str(", ")+option7 + str(" ") +option9]
+    else:
+        default_prompt = ["A photograph of a doctor or healthcare professional in a clinical setting, looking compassionate and confident while interacting with a patient. This image should convey a sense of trust and expertise."]
                 
             #prompt = st.text_input('Input your prompt here')
-        st.markdown("<h3 style='text-align: center; color: grey;'> Final Instruction for Image Generation </h3>", unsafe_allow_html=True)
-        prompt_design = st.warning(default_prompt[0],icon='🤖')
+    st.markdown("<h3 style='text-align: center; color: grey;'> Final Instruction for Image Generation </h3>", unsafe_allow_html=True)
+    prompt_design = st.warning(default_prompt[0],icon='🤖')
 
     API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
     headers = {"Authorization": "Bearer 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"}
