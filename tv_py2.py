aeed4d94b8fa3ed40817af132588adcaf312ce4e
YashSunidhi <smnitrkl50@gmail.com>
2023-10-15 15:32:35 +0200

    Update tv_py2.py

--- tv_py2.py
+++ tv_py2.py
@@ -393,7 +393,7 @@ def image_gen():
         response = generate_response(option8,hf_email, hf_pass)
         if response:
             torpedo = st.write(response)
-            st.session_state.messages_1.append(response)
+            st.session_state.messages_1.append(response.text)
 
     st.markdown("<h6 style='text-align: center; color: grey;'> Generated Image Placeholders from Finalized Text Generation Prompt </h6>", unsafe_allow_html=True)
     st.session_state.messages_1       
