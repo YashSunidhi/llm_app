64058c6c5fff61a84102bf31629e1283403eff99
YashSunidhi <smnitrkl50@gmail.com>
2023-10-16 00:58:36 +0200

    Update tv_py2.py

--- tv_py2.py
+++ tv_py2.py
@@ -130,7 +130,8 @@ def text_gen():
             option12 = st.sidebar.selectbox(
             'Company',
             ("Genentech's", "Roche's"))
-    
+            st.title("Prompt Design Template")
+            option7 = st.text_input('Input your prompt here',"")
 
             if option0C:
                 if pps:
@@ -140,9 +141,8 @@ def text_gen():
             else:
                 default_prompt = ["As a " + option0 +" expert, Write a " +option4 +" using tone of " + option11 + " in less than 3000 words for HCP/ doctors highlighting about " + option12 + option1+ "role in treatment and its potential benefits in terms of mechanism of action, safety, efficacy and clinical trials (trial name, trial objective ,trial dosing /formulation and trial outcome). Use an " +option3+ " tone. While generating outcome, please consider recent facts from year 2022 and 2023. The objective is to " + option5 + " to those seeking "+ option8+" options. " + option7]
                 #prompt = st.text_input('Input your prompt here')
-    st.title("Prompt Design Template")
-    option7 = st.text_input('Input your prompt here',"")
-    prompt_design = st.write(default_prompt[0])
+
+            prompt_design = st.write(default_prompt[0])
     
 
     
