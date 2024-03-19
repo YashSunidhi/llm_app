4037293d207b9643c2c09cbdc809e7430f41114a
YashSunidhi <smnitrkl50@gmail.com>
2024-03-19 14:50:34 +0100

    Update tv_py4.py

--- tv_py4.py
+++ tv_py4.py
@@ -245,7 +245,7 @@ def text_gen():
                             dv = pd.read_csv('content_output_Phesgo.csv')
                             response = dv['Outcome'][0]
                         elif option1=='evrysdi':
-                            dv = pdf_viewer("content_output_Ervysdi.csv")
+                            dv = pd.read_csv("content_output_Ervysdi.csv")
                             response = dv['Outcome'][0]
                         st.write(response)
                         message = {"role": "assistant", "content": response}
